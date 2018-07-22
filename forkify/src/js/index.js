6ef632215d9c74e484ea36382fcc34662c754e9e
bbaral <bbaral@asu.edu>
2018-07-22 19:36:56 -0400

    forkify

--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -1,4 +1,6 @@
 import Search from './model/Search';
+import * as searchView from './views/searchView';
+import {elements} from './views/base';
 
 /**
  * Global state of the app
@@ -11,18 +13,20 @@ const state = {};
 
 const controlSearch = async () => {
     //1) Get query from view
-    const query = 'pizza'; //TODO
+    const query = searchView.getInput(); //TODO
     if (query) {
         //2) New search object and add to state
         state.search = new Search(query);
         //3) Prepare UI for results
+        await searchView.clearInput();
+        await searchView.clearResult();
         //4) Search for recipes
         await state.search.getResults();
         //5) Render results on UI
-        console.log(state.search.result);
+        searchView.renderResults(state.search.result);
     }
 };
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch()
 });
